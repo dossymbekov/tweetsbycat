60ece40e16ee78f03e4a578d595c104a47ea0347
dossymbekov <berser91@gmail.com>
2019-02-11 11:39:12 +0000

    last updated version from server

--- tweetsbycat/settings.py
+++ tweetsbycat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r21w!5qz7!_*%@t_00(3fiw$0n%g_%7*6pw&f+f!2_*$*n84r8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tweetsbycat.pythonanywhere.com']
 
