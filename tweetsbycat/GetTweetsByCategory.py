055451814c17439d70ca4ef061a2747b060d4959
Almas <almas@Almass-MacBook-Pro.local>
2019-02-13 19:15:08 +0000

    getTweetsByCategory changed

--- tweetsbycat/GetTweetsByCategory.py
+++ tweetsbycat/GetTweetsByCategory.py
@@ -1,20 +1,21 @@
 import pandas as pd
 from .settings import BACKEND_DIR
 
-train_filepath = BACKEND_DIR + "/outs/valid.test"
-df = pd.read_table(train_filepath, encoding="ISO-8859-1", header=None)
 
-df.columns = ['tweet_id', 'text', 'classification']
-
-pred_filepath = BACKEND_DIR + "/svm_multiclass_linux64/pred3.out"
-#pred_filepath = BACKEND_DIR + "/svm_multiclass/pred3.out"
-df_p = pd.read_table(pred_filepath, encoding="ISO-8859-1", header=None, sep=" ", usecols=[0])
-
-df_new = pd.concat([df, df_p], axis=1)
-
-df_new.columns = ['ID', 'TWEET', 'CATEGORY', 'CAT']
 
 def getTweetsByCategory(cat):
+    train_filepath = BACKEND_DIR + "/outs/valid.test"
+    df = pd.read_table(train_filepath, encoding="ISO-8859-1", header=None)
+
+    df.columns = ['tweet_id', 'text', 'classification']
+
+    pred_filepath = BACKEND_DIR + "/svm_multiclass_linux64/pred3.out"
+    #pred_filepath = BACKEND_DIR + "/svm_multiclass/pred3.out"
+    df_p = pd.read_table(pred_filepath, encoding="ISO-8859-1", header=None, sep=" ", usecols=[0])
+
+    df_new = pd.concat([df, df_p], axis=1)
+
+    df_new.columns = ['ID', 'TWEET', 'CATEGORY', 'CAT']
     filter = 'CAT=='+cat
     result_df = df_new[['TWEET', 'CAT']].query(filter)
     res = list(result_df['TWEET'])
